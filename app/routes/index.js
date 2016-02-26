fe0b226e87aaaed8aa160e55590aae236b4f9783
NathanRomike <nathanromike@gmail.com>
2016-02-26 09:44:20 -0800

    Add hide show functions to new question form.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  questionForm: false,
+  actions: {
+    showQuestionForm() {
+      this.set('questionForm', true);
+    }
+  }
 });
