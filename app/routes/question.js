bb63bd8440054987dd15c4d184db11a198188891
NathanRomike <nathanromike@gmail.com>
2016-02-26 15:34:04 -0800

    Fix all answers displaying on all question pages.

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,14 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return Ember.RSVP.hash({
-      question: this.store.findRecord('question', params.question_id),
-      answers: this.store.findAll('answer', params.question_id)
+      question: this.store.findRecord('question', params.question_id)
     });
   },
 
   actions: {
     saveAnswer(params) {
-      console.log(params.question.id);
       var newAnswer = this.store.createRecord('answer', params);
       var question = params.question;
       question.get('answers').addObject(newAnswer);
